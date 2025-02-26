7a6ec65fe8730fc7cc3617d6c320bde0cdb27c46
mklNeu <11223037@st.neu.edu.vn>
2025-02-26 13:56:37 +0700

    bring back old responsive

--- src/pages/AdminBooks.jsx
+++ src/pages/AdminBooks.jsx
@@ -4,7 +4,14 @@ const AdminBooks = () => {
     return (
         <div className="flex w-full bg-gray-500">
             <SideBar />
-            <div className="flex w-full">
+            <div className="flex flex-col">
+                <div className="flex w-24 h-24 bg-yellow-400 grow  justify-center">siu</div>
+                <div className="flex w-24 h-24 bg-yellow-400 grow  justify-center">siu</div>
+                <div className="flex w-24 h-24 bg-yellow-400 grow  justify-center">siu</div>
+                <div className="flex w-24 h-24 bg-yellow-400 grow  justify-center">siu</div>
+                <div className="flex w-24 h-24 bg-yellow-400 grow  justify-center">siu</div>
+                <div className="flex w-24 h-24 bg-yellow-400 grow  justify-center">siu</div>
+                <div className="flex w-24 h-24 bg-yellow-400 grow  justify-center">siu</div>
                 <div className="flex w-24 h-24 bg-yellow-400 grow  justify-center">siu</div>
             </div>
         </div>
