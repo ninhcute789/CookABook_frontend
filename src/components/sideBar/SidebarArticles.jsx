f56dc61cad2e62e707a7498f65a53d19088333e9
mklNeu <11223037@st.neu.edu.vn>
2025-03-16 12:27:27 +0700

    minor changes

--- src/components/sideBar/SidebarArticles.jsx
+++ src/components/sideBar/SidebarArticles.jsx
@@ -1,12 +1,6 @@
 import PropTypes from "prop-types";
 
 const SidebarArticles = ({ newsArticles, setArticle }) => {
-  const truncateText = (text, wordLimit) => {
-    const words = text.split(" ");
-    return words.length > wordLimit
-      ? words.slice(0, wordLimit).join(" ") + "..."
-      : text;
-  };
   const truncateDate = (text, wordLimit) => {
     const words = text.split(" ");
     return words.length > wordLimit
@@ -14,7 +8,7 @@ const SidebarArticles = ({ newsArticles, setArticle }) => {
       : text;
   };
   return (
-    <aside className=" sticky top-15 z-50 mt-5 h-fit ml-auto w-3/8 hidden md:block  p-4 rounded-xl">
+    <aside className=" sticky top-0 z-50 mt-5 h-fit ml-auto w-3/8 hidden md:block  p-4 rounded-xl">
       <div className="w-fit mb-5">
         <h3 className="text-2xl font-bold mt-6 mb-3"> Bài viết nổi bật</h3>
         <hr className="text-amber-600 border-2 " />
